881755edbeb9975375fcd3b9acfa0c5c9a34fba0
Martin.E.Toftevag <martin.e.toftevag@student.uib.no>
2025-09-08 06:00:00 +0200

    added method for readable output on vote details

--- src/main/java/com/restApi/demo/controllers/VoteController.java
+++ src/main/java/com/restApi/demo/controllers/VoteController.java
@@ -2,11 +2,15 @@ package com.restApi.demo.controllers;
 
 import com.restApi.demo.domain.DomainManager;
 import com.restApi.demo.domain.Vote;
+import com.restApi.demo.domain.VoteOption;
+
 import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/polls/{pollId}")
@@ -23,14 +27,30 @@ public class VoteController {
   public VoteController(DomainManager domainManager){
     this.domainManager = domainManager;
   }
+  
+  // Return a readable vote object
+  private Map<String, Object> toView(Vote v) {
+    var poll = domainManager.getPoll(v.getPollId()).get();
+    var user = domainManager.getUser(v.getUserId()).get();
+    VoteOption option = domainManager.listOptions(poll.getId()).stream().filter(o -> o.getId().equals(v.getOptionId())).findFirst().get();
+
+    return Map.of(
+        "id", v.getId(),
+        "poll", Map.of("id", poll.getId(),"question", poll.getQuestion()),
+        "user", Map.of("id", user.getId(),"username", user.getUsername()),
+        "option", Map.of("id", option.getId(),"caption", option.getCaption()),
+        "publishedAt", v.getPublishedAt()
+    );
+  }
 
   @PostMapping("/votes")
-  public Vote upsert(@PathVariable UUID pollId, @RequestBody VoteReq request) {
-    return domainManager.castOrChangeVote(pollId,UUID.fromString(request.getUserId()),UUID.fromString(request.getOptionId()));
+  public Map<String, Object> upsert(@PathVariable UUID pollId, @RequestBody VoteReq req) {
+    var v = domainManager.castOrChangeVote(pollId, UUID.fromString(req.getUserId()), UUID.fromString(req.getOptionId()));
+    return toView(v);
   }
 
   @GetMapping("/votes")
-  public List<Vote> list(@PathVariable UUID pollId) {
-    return domainManager.listMostRecentVotes(pollId);
+  public List<Map<String, Object>> list(@PathVariable UUID pollId) {
+    return domainManager.listMostRecentVotes(pollId).stream().map(this::toView).collect(Collectors.toList());
   }
 }
